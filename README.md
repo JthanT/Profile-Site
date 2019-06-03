4403e6b40e8de55eeee3655ec19a817e03d93a6e
Johnathan Tam <>
2019-06-03 17:23:18 -0600

    Removed some placeholder code and updated info in package.json.

--- README.md
+++ README.md
@@ -1,34 +1,8 @@
-# MERN-boilerplate
+# Profile-Site
 
-This is a boilerplate project using the following technologies:
-- [React](https://facebook.github.io/react/) and [React Router](https://reacttraining.com/react-router/) for the frontend
-- [Express](http://expressjs.com/) and [Mongoose](http://mongoosejs.com/) for the backend
-- [Sass](http://sass-lang.com/) for styles (using the SCSS syntax)
-- [Webpack](https://webpack.github.io/) for compilation
+This is a user profile project that uses MongoDB, Express, React, and Node. 
 
+Initial setup of this project was made using the "MERN-boilerplate" located here:
+https://github.com/arkon/MERN-boilerplate.git
 
-## Requirements
-
-- [Node.js](https://nodejs.org/en/) 6+
-
-```shell
-npm install
-```
-
-
-## Running
-
-Make sure to add a `config.js` file in the `config` folder. See the example there for more details.
-
-Production mode:
-
-```shell
-npm start
-```
-
-Development (Webpack dev server) mode:
-
-```shell
-npm run start:dev
-```
 # Profile-Site
--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,79 +1,13 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
-import { getFromStorage, setInStorage } from '../../utils/storage';
+// import { getFromStorage, setInStorage } from '../../utils/storage';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoading: true,
-      token: '',
-      signUpError: '',
-      signInError: ''
-    }
-  }
-
-  componentDidMount() {
-    const token = getFromStorage('profile_app');
-
-    if(token){
-      fetch('/api/account/verify?token=' + token)
-        .then(res => res.json())
-        .then(json => {
-          if(json.success) {
-            this.setState({
-              token: token,
-              isLoading: false,
-            });
-          } else {
-            this.setState({ isLoading: false });
-          }
-        });
-    } else {
-      this.setState({ isLoading: false });
-    }
-  }
-
-  newCounter() {
-    fetch('/api/counters', { method: 'POST' })
-      .then(res => res.json())
-      .then(json => {
-        let data = this.state.counters;
-        data.push(json);
-
-        this.setState({
-          counters: data
-        });
-      });
-  }
-
   render() {
-    const {
-      isLoading,
-      token,
-    } = this.state;
-
-    if(isLoading) {
-      return(
-        <div>
-          <p>Loading...</p>
-        </div>
-      );
-    }
-
-    if(!token) {
-      return(
-        <div>
-          <p>Sign In</p>
-          <p>Sign Up</p>
-        </div>
-      );
-    }
-
     return (
-      <div>
-        <p>Account</p>
+      <div className="container">
+        <h3>React Form</h3>
+        <FormContainer />
       </div>
     );
   }
