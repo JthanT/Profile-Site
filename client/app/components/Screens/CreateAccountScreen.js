479f1217ade5127b05fc1b278910ca6f62abbf31
Johnathan Tam <>
2019-06-05 16:05:16 -0600

    Added registration fields to the create account screen.
    The create account fields do not do anything yet.

--- client/app/components/Screens/CreateAccountScreen.js
+++ client/app/components/Screens/CreateAccountScreen.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
 import 'whatwg-fetch';
 // import { getFromStorage, setInStorage } from '../../utils/storage';
 
@@ -6,20 +8,39 @@ class CreateAccountScreen extends Component {
   render() {
     return (
       <div className="create_account_screen">
-        <div className="create_account_form">
-          <h1>Create account</h1>
-          // <form onSubmit={this.saveAndContinue}>
-          <form onSubmit={this.saveAndContinue}>
-            <Input
-                text="First Name"
-                ref="firstName"
-                validate={this.isEmpty}
-                value={this.state.firstName}
-                // onChange={this.handleCompanyInput}
-                // emptyMessage="Company name can't be empty"
+        <MuiThemeProvider>
+          <div>
+            <TextField
+              hintText="Enter your First Name"
+              floatingLabelText="First Name"
+              onChange = { (event,newValue) => this.setState({ firstName:newValue }) }
             />
-          </form>
-        </div>
+            <br/>
+            <TextField
+              hintText="Enter your Last Name"
+              floatingLabelText="Last Name"
+              onChange = { (event,newValue) => this.setState({ lastName:newValue }) }
+            />
+            <br/>
+            <TextField
+              hintText="Enter your Email"
+              floatingLabelText="Email"
+              onChange = { (event,newValue) => this.setState({ email:newValue }) }
+            />
+            <br/>
+            <TextField
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange = { (event,newValue) => this.setState({ username:newValue }) }
+            />
+            <br/>
+            <TextField
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange = { (event,newValue) => this.setState({ password:newValue }) }
+            />
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
