6589d2e102d69eb631f6d5fe492030a3b99c22a0
Johnathan Tam <>
2019-06-11 12:44:31 -0600

    Updated packages and added submit button with some functionality.
    The submit button has base code for post requests to the server.

--- client/app/components/Screens/CreateAccountScreen.js
+++ client/app/components/Screens/CreateAccountScreen.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import 'whatwg-fetch';
-// import { getFromStorage, setInStorage } from '../../utils/storage';
 
 class CreateAccountScreen extends Component {
+  signUpEvent() {
+    const {
+      signUpEmail,
+      signUpPassword,
+    } = this.state;
+
+    fetch('/api/account/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: signUpEmail,
+        password: signUpPassword,
+      }),
+    }).then(res => res.json())
+      .then(json => {
+        console.log('json', json);
+        if (json.success) {
+          this.setState({
+            signUpError: json.message,
+            signUpEmail: '',
+            signUpPassword: '',
+          });
+        } else {
+          this.setState({
+            signUpError: json.message,
+          });
+        }
+      });
+  }
+
   render() {
     return (
       <div className="create_account_screen">
@@ -39,6 +71,8 @@ class CreateAccountScreen extends Component {
               floatingLabelText="Password"
               onChange = { (event,newValue) => this.setState({ password:newValue }) }
             />
+            <br/>
+            <RaisedButton label="Submit" primary={ true } onClick={ (event) => this.signUpEvent(event) }/>
           </div>
         </MuiThemeProvider>
       </div>
